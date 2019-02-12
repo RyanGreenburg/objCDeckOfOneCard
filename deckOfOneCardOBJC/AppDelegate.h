31fee674afccdedcba2fc87d9a1c51990ab8ccc1
Ryan Greenburg <ryangreenburg@mac.com>
2019-02-12 13:22:05 -0700

    Initial Commit

---
+++ deckOfOneCardOBJC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  deckOfOneCardOBJC
+//
+//  Created by RYAN GREENBURG on 2/12/19.
+//  Copyright © 2019 RYAN GREENBURG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
